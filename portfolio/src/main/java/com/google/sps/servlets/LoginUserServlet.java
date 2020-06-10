b8e6e753e0418dc8db3767d9b0763d55b466abe2
Grant <grantcommodore24@gmail.com>
2020-06-10 23:05:56 +0000

    Fixed logout and login method mixup

--- portfolio/src/main/java/com/google/sps/servlets/LoginUserServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginUserServlet.java
@@ -43,9 +43,9 @@ public class LoginUserServlet extends HttpServlet {
     User usr;
     
     if (loggedIn) {
-      usr = new User(loggedIn,userService.createLoginURL(URL)); 
+      usr = new User(loggedIn,userService.createLogoutURL(URL)); 
     } else {
-      usr = new User(loggedIn, userService.createLogoutURL(URL));
+      usr = new User(loggedIn, userService.createLoginURL(URL));
     }
     response.getWriter().println(new Gson().toJson(usr));
   } 
