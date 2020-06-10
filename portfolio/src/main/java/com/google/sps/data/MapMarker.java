f8872583c6536e7b9d728a9c161b3e18d0dea858
Grant <grantcommodore24@gmail.com>
2020-06-10 11:08:40 -0400

    created the mapmarker class

---
+++ portfolio/src/main/java/com/google/sps/data/MapMarker.java
@@ -0,0 +1,26 @@
+package com.google.sps.data;
+
+import com.google.appengine.api.datastore.Entity;
+
+public final class MapMarker {
+  private final String collegeName; 
+  private final String internName; 
+  private final long longitude; 
+  private final long latitude; 
+
+  public MapMarker(String collegeName, String internName, long longitude, long latitude) {
+    this.collegeName = collegeName; 
+    this.internName = internName; 
+    this.longitude = longitude;
+    this.latitude = latitude; 
+  }
+
+  public Entity makeEntity(){
+    Entity mapMarkerEntity = new Entity("mapMarker");
+    mapMarkerEntity.setProperty("collegeName", this.collegeName);
+    mapMarkerEntity.setProperty("internName", this.internName);
+    mapMarkerEntity.setProperty("longitude", this.longitude);
+    mapMarkerEntity.setProperty("latitude", this.latitude);
+    return mapMarkerEntity; 
+  }
+}
